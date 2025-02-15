05dbca6c35aceb6f538c6532f710bda4501c2487
thcerutti <thiagocerutti@gmail.com>
2025-02-15 17:44:39 -0300

    feat: adiciona dados para a seção Lançamentos

--- src/app/components/Highlights/Highlights.jsx
+++ src/app/components/Highlights/Highlights.jsx
@@ -1,5 +1,19 @@
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
+
+import style from "./Highlights.module.css";
+
 const Header = () => {
-  return <h1>Highlights</h1>;
+  return (
+    <div className={style.container}>
+      <div className={style.anterior}>
+        <FaAngleLeft />
+      </div>
+      <div className={style.conteudo}></div>
+      <div className={style.proximo}>
+        <FaAngleRight />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/app/components/Lancamentos/Lancamentos.jsx
+++ src/app/components/Lancamentos/Lancamentos.jsx
@@ -1,5 +1,23 @@
+import AnimeCard from "../AnimeCard";
+import style from "./Lancamentos.module.css";
+import db from "@/app/db.json";
+
 const Lancamentos = () => {
-  return <h1>Lançamentos</h1>;
+  return (
+    <div className={style.container}>
+      <h2>Lançamentos</h2>
+      <div>
+        {db.animes.map((anime) => (
+          <AnimeCard
+            key={anime.id}
+            titulo={anime.nome}
+            anoLancamento={anime.ano}
+            imagem={anime.capa}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Lancamentos;
